f614021db11fcaff6a9237c187ebc3c4680aa09e
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-07-01 14:46:29 +0500

    added

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -10,7 +10,7 @@ const Heading = ({
 }) => {
   return (
     <div>
-      <h1 className={`text-4xl md:text-6xl font-semibold mb-4 ${titleColor}`} style={{ lineHeight: '1.25' }}>
+      <h1 className={`text-4xl md:text-6xl font-bold mb-4 ${titleColor}`} style={{ lineHeight: '1.25' }}>
         {title}
       </h1>
       <p className={`text-lg md:text-xl mb-8 ${textColor}`}>
