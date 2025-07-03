0873569f6a370d090b0a0bca7dd1e8464596573b
Muhammad Omer Baig <engromerbaig@gmail.com>
2025-07-03 17:20:28 +0500

    added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,26 +7,23 @@ import Heading from '@/components/Heading/Heading';
 import FadeWrapper from '@/utilities/Animations/FadeWrapper';
 import ScrollSamePage from '@/utilities/Scroll/ScrollSamePage';
 
+import Link from 'next/link';
+
 const Hero = () => {
   return (
     <section className={`${theme.hero.bgColor} ${theme.sectionPaddings.horizontalPx} py-12 font-sans`}>
       <div className="container mx-auto flex flex-col gap-6 md:flex-row items-center">
         <div className="flex flex-col w-full lg:w-1/2 justify-center items-start">
-          <Heading
-            title="From Pixels to Logic: Where Ideas Evolve into Innovation"
-            text="I design, develop, and deploy stunning web applications, turning ideas into reality seamlessly. Let's discuss your project!"
-          />
+       <Heading
+  title="Full-Stack Web Solutions with MERN Precision"
+  text="I specialize in building fast, scalable, and responsive web applications using the MERN stack. From frontend design to backend logic, I turn ideas into production-ready digital experiences."
+/>
+
 
           <div className="flex flex-col items-center justify-start w-full md:flex-row gap-4">
-            <FadeWrapper order={1}>
-              <Button
-                text="Contact Me"
-                to="/contact"
-                hover
-              />
-            </FadeWrapper>
 
-            <FadeWrapper order={2}>
+
+             <FadeWrapper order={1}>
               <ScrollSamePage targetId="projects">
                 <Button
                   text="MY PROJECTS"
@@ -34,6 +31,15 @@ const Hero = () => {
                 />
               </ScrollSamePage>
             </FadeWrapper>
+
+
+            <FadeWrapper order={2}>
+<Link href="/resume.pdf" passHref legacyBehavior>
+  <a download className="bg-theme-blue hover:bg-theme-blue/80 text-white py-4 uppercase px-4 rounded font-bold">My Resume</a>
+</Link>
+            </FadeWrapper>
+
+           
           </div>
         </div>
 
@@ -52,7 +58,8 @@ const Hero = () => {
           />
           <div className="absolute w-3/4 lg:w-1/2 bottom-0 lg:left-12 bg-theme-blue bg-opacity-100 p-3 shadow-lg">
             <p className="text-white text-sm">
-              "Alone we can do so little; together we can do so much." – Helen Keller
+“First, solve the problem. Then, write the code.” – John Johnson
+
             </p>
           </div>
         </FadeWrapper>
